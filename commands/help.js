969ce06205ec4b1fe9f17c387843f437f1bb2843
OfficialCRUGG <mrcreeper2011@web.de>
2018-07-21 13:07:40 +0200

    Changes to help

--- commands/help.js
+++ commands/help.js
@@ -1,9 +1,10 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (prefix, messageArray, cmd, bot, message, args, author, guild, lang) => {
-  return message.channel.send(`embed`)
+  //let embed = new Discord.RichEmbed();
+  //return message.channel.send(embed);
 }
 
 module.exports.help = {
-  name: "invite"
+  name: "help"
 }
