0679030b2bc0591c957e16cdba0ef79da04f860a
OfficialCRUGG <mrcreeper2011@web.de>
2018-11-11 17:08:26 +0100

    Added unserinfo command

---
+++ commands/userinfo.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (prefix, messageArray, cmd, bot, message, args, author, guild, lang) => {
+      //let gicon = message.guild.iconURL;
+      //let gname = message.guild.name;
+      let user = message.mentions.users.first();
+      let embed = new Discord.RichEmbed()
+      embed.setTitle(user.username + lang.userInformation.title)
+      embed.setColor("#7289DA")
+      embed.setThumbnail(user.avatarURL)
+      embed.setAuthor(message.author.username + "#" + message.author.discriminator, message.author.avatarURL)
+      embed.addField(lang.userInformation.info.userName, user.tag)
+      embed.addField(lang.userInformation.info.createdAt, user.createdAt)
+      embed.addField(lang.userInformation.info.userID, user.id)
+      if(user.presence.status == "online") {
+          embed.addField(lang.userInformation.info.status, lang.userInformation.statuses.online)
+      } else if(user.presence.status == "offline") {
+          embed.addField(lang.userInformation.info.status, lang.userInformation.statuses.offline)
+      } else if(user.presence.status == "dnd") {
+          embed.addField(lang.userInformation.info.status, lang.userInformation.statuses.dnd)
+      } else if(user.presence.status == "idle") {
+          embed.addField(lang.userInformation.info.status, lang.userInformation.statuses.idle)
+      } else {
+          embed.addField(lang.userInformation.info.status, "Error")
+      }
+      if(user.presence.game == null) {
+
+      } else {
+          embed.addField(lang.userInformation.info.game, user.presence.game.name)
+      }
+
+      return message.channel.send(embed);
+}
+
+module.exports.help = {
+  name: "userinfo"
+}
