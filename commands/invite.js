7ddd1f47c628af4f17261e60a975cd4f85537c70
OfficialCRUGG <mrcreeper2011@web.de>
2018-12-16 18:49:43 +0100

    v1.2.3 Removed invite command as it wasn't really needed

--- commands/invite.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (prefix, messageArray, cmd, bot, message, args, author, guild, lang, config) => {
-  return message.channel.send(`${lang.inviteCommand}`)
-}
-
-module.exports.help = {
-  name: "invite"
-}
