db652adc083529dc80c05ec16705f40260d77af9
OfficialCRUGG <mrcreeper2011@web.de>
2018-12-16 10:44:38 +0100

    Added help command

--- commands/help.js
+++ commands/help.js
@@ -1,8 +1,14 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (prefix, messageArray, cmd, bot, message, args, author, guild, lang, config) => {
-  //let embed = new Discord.RichEmbed();
-  //return message.channel.send(embed);
+  let embed = new Discord.RichEmbed()
+  .setTitle(lang.help.title)
+  .setColor(config.mainColor)
+  .setAuthor(message.author.username + "#" + message.author.discriminator, message.author.avatarURL)
+  .addField(config.prefix + "invite", lang.help.commandDescriptions.invite)
+  .addField(config.prefix + "guildinfo", lang.help.commandDescriptions.guildinfo)
+  .addField(config.prefix + "userinfo", lang.help.commandDescriptions.userinfo)
+  return message.channel.send(embed);
 }
 
 module.exports.help = {
